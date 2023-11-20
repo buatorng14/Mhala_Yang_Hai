ef5859f0841154da21d26456562c7221ce53884a
buatorng14 <150588311+buatorng14@users.noreply.github.com>
2023-11-20 17:35:01 +0700

    สักทีแม่

--- seller.py
+++ seller.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import mysql.connector
-import ast
 
 # Connect to MySQL database
-mydb =  mysql.connector.connect(
+mydb = mysql.connector.connect(
         host="139.5.147.31",
         port="3306",
         user="trong",
@@ -17,25 +16,30 @@ data = cursor.fetchall()
 
 st.title('รายการคำสั่งซื้อทั้งหมด')
 
+# Use st.form() outside the loop to manage the form submission
+form = st.form(key='delete_form')
+
 for product in data:
     OrderCode, Product, TotalPrice, CustomerNote = product
-    with st.form(key=f'form_{OrderCode}'):
+    with form:
         st.write(OrderCode)
         st.write(Product)
         st.write(f'**ราคารวม:** :red[{TotalPrice} ฿]')
         st.write(f'**โน้ตถึงร้านค้า:** {CustomerNote}')
         
-        if st.form_submit_button(label='Finish', use_container_width=True):
+        if form.form_submit_button(label=f'Delete {OrderCode}', help=f'Click to delete order {OrderCode}'):
             # Insert into history_order table
             cursor.execute("INSERT INTO history_order (ordercode) VALUES (%s)", (OrderCode,))
-            mydb.commit()
-            
             # Delete from orders table
             cursor.execute("DELETE FROM customer_order WHERE OrderCode=%s", (OrderCode,))
-            mydb.commit()
-            
-            st.toast(f'Order {OrderCode} deleted successfully! The order is ready for pickup.', icon='❎')
+    
+# Commit changes outside the loop
+mydb.commit()
 
-# Close the cursor and database connection outside the loop
+# Show success message if deletion is successful
+if form.form_submit_button(label='Finish', help='Finish deleting orders'):
+    st.success("Selected orders deleted successfully!")
+
+# Close the cursor and database connection
 cursor.close()
 mydb.close()
