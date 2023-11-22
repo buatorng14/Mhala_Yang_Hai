20a0572394b3fc389685b2cf08d685bd56d58bc8
buatorng14 <150588311+buatorng14@users.noreply.github.com>
2023-11-22 13:54:43 +0700

    Update seller.py

--- seller.py
+++ seller.py
@@ -8,8 +8,7 @@ mydb = mysql.connector.connect(
         port="3306",
         user="trong",
         password="c757GL28zN",
-        database="trong",
-        ssl_disabled=True
+        database="trong"
 )
 cursor = mydb.cursor()
 cursor.execute("SELECT OrderCode, Product, TotalPrice, CustomerNote FROM customer_order")
