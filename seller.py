9cd9547db4d4ef1a3275e8e67eeb1bee2641d609
buatorng14 <150588311+buatorng14@users.noreply.github.com>
2023-11-22 14:13:18 +0700

    Update seller.py

--- seller.py
+++ seller.py
@@ -5,7 +5,7 @@ import ast
 # Connect to MySQL database
 mydb = mysql.connector.connect(
     host="139.5.147.31",
-    port="3306",
+    port=3306,
     user="trong",
     password="c757GL28zN",
     database="trong",
